afc949c8615d0dbf60e67f0874ebaf67792ecf18
Janay05 <janaybhanushali.05@gmail.com>
2026-01-13 20:23:43 +0530

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+struct TlbEntry {
+    tag: u64,
+    pfn: u64,
+    valid: bool,
+    last_access: u64,
+}
+
+struct Tlb {
+    sets: Vec<Vec<TlbEntry>>,
+    num_sets: usize,
+    associativity: usize,
+
+    //Stats
+    hits: u64,
+    misses: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    //awdda
 }
