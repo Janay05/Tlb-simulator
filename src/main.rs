1881617111938bb6a55302b6d95281375c550bbd
Janay05 <janaybhanushali.05@gmail.com>
2026-01-19 03:57:08 +0530

    lookup function

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,33 @@ struct Tlb {
     hits: u64,
     misses: u64,
 }
+impl Tlb {
+    fn get_indices(&self, address: u64) -> (usize, u64) {
+        let offset_bits = 12; //Assuming a standard 4KB page
+
+        let index = (address >> offset_bits) as usize % self.num_sets; //(address >> offset_bits) as usize & (self.num_sets - 1) this is faster than using % as it simulates the absolute speed of the hardware
+        let index_bits = (self.num_sets as f64).log2() as u32;
+        let tag = address >> (offset_bits + index_bits); 
+
+        (index,tag) 
+
+
+    }
+    pub fn lookup(&mut self, address: u64) -> bool {
+
+        let (index,tag) = self.get_indices(address);
+        for entry in &mut self.sets[index] {
+            if tag == entry.tag && entry.valid == true {
+                self.hits = self.hits + 1 ;
+                //update the last_access variable
+                return true;
+            }
+        }
+        self.misses = self.misses + 1 ;
+        return false;
+    }
+ }
 
 fn main() {
-    //awawdaddda
+
 }
