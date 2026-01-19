4d00d32c28309f7c4a9dcc6942bc356341de757e
Janay05 <janaybhanushali.05@gmail.com>
2026-01-19 16:39:13 +0530

    insert function

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,9 @@ struct Tlb {
     //Stats
     hits: u64,
     misses: u64,
+
+    //Global Clock variable for LRU
+    timer: u64,
 }
 impl Tlb {
     fn get_indices(&self, address: u64) -> (usize, u64) {
@@ -32,12 +35,35 @@ impl Tlb {
         for entry in &mut self.sets[index] {
             if tag == entry.tag && entry.valid == true {
                 self.hits = self.hits + 1 ;
-                //update the last_access variable
+                self.timer += 1;
+                entry.last_access = self.timer;//update the last_access variable
                 return true;
             }
         }
+        self.timer += 1;
         self.misses = self.misses + 1 ;
         return false;
+    }
+    pub fn insert(&mut self, address: u64, pfn: u64) -> () {
+        let (index,tag) = self.get_indices(address);
+        let mut victim_way: usize = 0;
+        let mut min_time = u64::MAX;
+        for (way_index,entry) in self.sets[index].iter_mut().enumerate() {
+            if entry.valid == false {
+                victim_way = way_index;
+                break;
+            }
+            else {
+                if entry.last_access < min_time {
+                    min_time = entry.last_access;
+                    victim_way = way_index;
+                }
+            }
+        }
+        self.sets[index][victim_way].last_access = self.timer;
+        self.sets[index][victim_way].tag = tag;
+        self.sets[index][victim_way].pfn = pfn;
+        self.sets[index][victim_way].valid = true;
     }
  }
 
